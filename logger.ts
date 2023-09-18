e94d4c20ef159c8cab9aea308c19e715bea5782f
Or Assael <or.assael@healthy.io>
2023-09-18 14:53:15 +0300

    Fixed PR comments

--- logger.ts
+++ logger.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as stream from "stream";
 import * as util from "util";
 
-import { once } from "events";
+import { promisify } from "util";
 
 enum LogDestination {
   Console,
@@ -13,7 +13,7 @@ class Logger {
   private logDestination: LogDestination;
   private logFileName: string;
   private logFileStream: fs.WriteStream;
-  private defaultLogger: Logger | undefined;
+  private static defaultLogger: Logger | undefined;
 
   // This is a promisified version of stream.finished to wait for the stream to finish writing
   private streamFinshed = util.promisify(stream.finished);
@@ -42,11 +42,11 @@ class Logger {
    * call this method to get a singleton instance of the Logger class
    * @returns {Logger} A singleton instance of the Logger class
    */
-  getLogger() {
-    if (!this.defaultLogger) {
-      this.defaultLogger = new Logger();
+  static getLogger(): Logger {
+    if (!Logger.defaultLogger) {
+      Logger.defaultLogger = new Logger();
     }
-    return this.defaultLogger;
+    return Logger.defaultLogger;
   }
 
   async debug(
@@ -54,14 +54,12 @@ class Logger {
     logDestination?: LogDestination,
     logFileName?: string
   ) {
-    if (this._isLogToConsole(logDestination)) {
-      console.debug(message);
-    } else {
-      await this._logToFile(
-        this._formatMessageForFile(message, this.logPrefix.debug),
-        logFileName
-      );
-    }
+    this._logToDestination(
+      message,
+      this.logPrefix.debug,
+      logDestination,
+      logFileName
+    );
   }
 
   async info(
@@ -69,14 +67,12 @@ class Logger {
     logDestination?: LogDestination,
     logFileName?: string
   ) {
-    if (this._isLogToConsole(logDestination)) {
-      console.info(message);
-    } else {
-      await this._logToFile(
-        this._formatMessageForFile(message, this.logPrefix.info),
-        logFileName
-      );
-    }
+    this._logToDestination(
+      message,
+      this.logPrefix.info,
+      logDestination,
+      logFileName
+    );
   }
 
   async warn(
@@ -84,14 +80,12 @@ class Logger {
     logDestination?: LogDestination,
     logFileName?: string
   ) {
-    if (this._isLogToConsole(logDestination)) {
-      console.warn(message);
-    } else {
-      await this._logToFile(
-        this._formatMessageForFile(message, this.logPrefix.warn),
-        logFileName
-      );
-    }
+    this._logToDestination(
+      message,
+      this.logPrefix.warn,
+      logDestination,
+      logFileName
+    );
   }
 
   async error(
@@ -99,14 +93,12 @@ class Logger {
     logDestination?: LogDestination,
     logFileName?: string
   ) {
-    if (this._isLogToConsole(logDestination)) {
-      console.error(message);
-    } else {
-      await this._logToFile(
-        this._formatMessageForFile(message, this.logPrefix.error),
-        logFileName
-      );
-    }
+    this._logToDestination(
+      message,
+      this.logPrefix.error,
+      logDestination,
+      logFileName
+    );
   }
 
   changeLogDestination(
@@ -125,17 +117,23 @@ class Logger {
     }
   }
 
-  private async _logToFile(message: string, logFileName?: string) {
+  private async _logToFile(
+    message: string,
+    prefix: string,
+    logFileName?: string
+  ) {
+    const formattedMessage = this.formatMessageForFile(message, prefix);
+
     if (logFileName && logFileName !== this.logFileName) {
       const tempFIleStream = fs.createWriteStream(logFileName, {
         encoding: "utf-8",
         flags: "a",
       });
-      await this._logToFileByChunks(message, tempFIleStream);
+      await this._logToFileByChunks(formattedMessage, tempFIleStream);
       tempFIleStream.end();
       await this.streamFinshed(tempFIleStream);
     } else {
-      await this._logToFileByChunks(message, this.logFileStream);
+      await this._logToFileByChunks(formattedMessage, this.logFileStream);
     }
   }
 
@@ -144,14 +142,46 @@ class Logger {
     fileStream: fs.WriteStream
   ) {
     for await (const chunk of message) {
-      if (!fileStream.write(chunk)) {
-        await once(fileStream, "drain");
-      }
+      const pipeline = promisify(stream.pipeline);
+      pipeline(chunk, fileStream).catch((err) => {
+        console.error("Pipeline failed.", err);
+        fileStream.end();
+      });
     }
   }
 
-  private _formatMessageForFile(message: string, prefix: string): string {
+  public formatMessageForFile(message: string, prefix: string): string {
     return prefix + message + "\n";
+  }
+
+  private async _logToDestination(
+    message: string,
+    prefix: string,
+    logDestination?: LogDestination,
+    logFileName?: string
+  ) {
+    if (this._isLogToConsole(logDestination)) {
+      this._logToConsoleWrapper(message, prefix);
+    } else {
+      await this._logToFile(message, prefix, logFileName);
+    }
+  }
+  // This is done for testing purposes
+  private _logToConsoleWrapper(message: string, prefix: string) {
+    switch (prefix) {
+      case this.logPrefix.debug:
+        console.debug(message);
+        break;
+      case this.logPrefix.info:
+        console.info(message);
+        break;
+      case this.logPrefix.warn:
+        console.warn(message);
+        break;
+      case this.logPrefix.error:
+        console.error(message);
+        break;
+    }
   }
 }
 
